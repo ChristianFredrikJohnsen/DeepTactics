1438989c194c585c4aa1737c35c08d9dfa4026af
Christian <cfj@johnsen.no>
2023-10-24 18:01:16 +0200

    Co-authored-by: Brage Kvamme <BrageHK@users.noreply.github.com> Co-authored-by: kristiancarlenius <kristiancarlenius@users.noreply.github.com>

--- Connect_four2/Connect_four.py
+++ Connect_four2/Connect_four.py
@@ -9,8 +9,10 @@ class ConnectFour:
         self.turn = 0
         self.running = True
         self.action_space = 7
+        self.reset()
 
-    def drop_piece(self, row, col, piece):
+    def drop_piece(self, col, piece):
+        row = self.get_next_open_row(col)
         self.board[row][col] = piece
 
     def is_valid_location(self, col):
@@ -34,7 +36,7 @@ class ConnectFour:
             for r in range(self.ROW_COUNT - 3):
                 if self.board[r][c] == piece and self.board[r+1][c] == piece and self.board[r+2][c] == piece and self.board[r+3][c] == piece:
                     return True
-
+    
         # Check positively sloped diagonals
         for c in range(self.COLUMN_COUNT - 3):
             for r in range(self.ROW_COUNT - 3):
@@ -50,28 +52,6 @@ class ConnectFour:
     def reset(self):
         self.board = np.zeros((self.ROW_COUNT, self.COLUMN_COUNT))
         return self.board.flatten()
-    
-
-    def generate_action(self):
-        self.turn += 1
-        legal_moves = [x for x in range(7) if self.is_valid_location(x)]
-        action = np.random.choice(legal_moves)
-        row = self.get_next_open_row(action)
-        piece = -1 if self.turn % 2 == 0 else 1
-        self.drop_piece(row, action, piece)
-
-        if self.winning_move(-1 * piece):
-            return (self.board.flatten(), -1, True)
-        if self.turn == 42:
-            return (self.board.flatten(), 0, True)
-        else:
-            if piece == 1:
-                return (self.board.flatten(), 0, False)
-            
-            else:
-                return (self.board.flatten() * -1, 0, False)
-
-        #return action
 
     def step(self, action):
         """
@@ -81,22 +61,15 @@ class ConnectFour:
         """
         
         self.turn += 1
-        row = self.get_next_open_row(action)
         piece = -1 if self.turn % 2 == 0 else 1
-        self.drop_piece(row, action, piece)
+        self.drop_piece(action, piece)
+        outputBoard = self.board.flatten()
         
-
         if self.winning_move(piece):
-            if piece == 1:
-                return (self.board.flatten(), 1, True)
-            else:
-                return (self.board.flatten() * -1, 1, True)
+            return (outputBoard, 1, True)
+        if self.turn == 42: # Draw
+            return (outputBoard, 0, True)
+        else:
+            return (outputBoard, 0, False)
         
-        if self.turn == 42:
-            return (self.board.flatten(), 0, True)
-        
-        return self.generate_action()
-        
-       
-if __name__ == "__main__":
-    print("NEIN")
+    
