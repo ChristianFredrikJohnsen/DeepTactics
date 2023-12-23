90f64ec45c4a1041c259c01003c6bec63c97b5ba
Christian Fredrik Johnsen <cjohnsen41@gmail.com>
2023-12-23 07:46:16 +0100

    Opponent is now better by default. It plays a winning move if it can, and it blocks the model if it has the ability to.
    Episode generation is considerably slower, since you need to have logic which checks if the opponent actually has a winning move, and if not, you need to check if the opponent has a legal blocking move.
    
    And in the end, if something about the DQN algorithm is not changed (we will probably need a mix of opponents instead of just 1), then the performance will probably just cap at this one-mover level.
    
    TLDR: This commit is a short-term solution to a big problem.

--- Connect_four2/README.md
+++ Connect_four2/README.md
@@ -1,4 +1,5 @@
-# Play connect 4 against a neural network trained with DQN and self play.
+# Play connect 4 against a neural network trained with DQN and self play
 
 ## HOW TO PLAY
+
 Navigate to gaming.py, create a new ConnectFourPygame object and run the run_with_bot([model-name])-method.
--- Connect_four2/env.py
+++ Connect_four2/env.py
@@ -1,4 +1,5 @@
 import torch
+from QNetwork import QNetwork
 from debug_utils.print_board import print_state
 
 class ConnectFourEnvironment():
@@ -59,6 +60,44 @@ class ConnectFourEnvironment():
                     if i > 0: # It is no longer possible to win in this direction.
                         break
         return False
+    
+    def potential_winning_move(self, piece, col, row):
+        """
+        Checks if the player has won.
+
+        NOTE:
+        We have tried to make this as efficient as possible, but it is still not very fast.
+        The reason why we want this to be efficient is because the method is called every time a player makes a move,
+        and when you do hundreds of thousands of iterations, it adds up.
+        """
+
+        if self.turn < 6:
+            return False  # It is not possible to win before turn 6.
+        
+        row = self.drop_piece(col, piece) # Drop the piece in the first empty row.
+        
+        directions = [(0, 1), (1, 0), (1, 1), (1, -1)] # Iterate over all directions, horizontal, vertical, diagonal up and diagonal down.
+        for dr, dc in directions:
+            count = 0
+            
+            for i in range(-3, 4):
+                r, c = row + i * dr, col + i * dc
+                if 0 <= r < self.HEIGHT and 0 <= c < self.WIDTH and self.board[r][c] == piece: # Checking if coordinate is within the board and if the piece is the same as the one we are looking for.
+                    count += 1
+                    if count == 4: # If we have had four consecutive pieces in a row, we have won.
+                        self.board[row][col] = 0 # Remove the piece from the board.
+                        self.row_cache[col] -= 1 # Undo the drop_piece method.
+                        return True
+                else:
+                    count = 0
+                    if i > 0: # It is no longer possible to win in this direction.
+                        break
+        
+        # The possible move was not a winning move, so we must undo the drop_piece method.
+        self.board[row][col] = 0 # Remove the piece from the board.
+        self.row_cache[col] -= 1 # Undo the drop_piece method.
+
+        return False
 
     def update_legal_moves(self):
         """
@@ -81,6 +120,38 @@ class ConnectFourEnvironment():
         action = torch.argmax(qvals).item() # Get the action with the highest Q-value among the legal moves.
 
         return action
+
+    def opponent_winning_move(self, piece):
+        """
+        Checks if the opponent has a winning move.
+        If it does, then it plays it.
+        """
+        for i in range(self.action_space):
+            if self.is_valid_location(i) and self.potential_winning_move(piece, i, self.row_cache[i]):
+                self.drop_piece(i, piece)
+                next_state = self.get_correct_board_representation(piece)
+                return (next_state * (-1), -1, True) # Opponent won, so player gets reward -1.
+            
+        return None # No winning move was found.
+
+    def player_winning_move(self, piece):
+        """
+        Checks if the player has a winning move (The agent who trains).
+        If the player has a winning move, the opponent must block it.
+        """
+        opponent_piece = piece * (-1)
+        for i in range(self.action_space):
+            
+            if self.is_valid_location(i) and self.potential_winning_move(opponent_piece, i, self.row_cache[i]):
+                self.drop_piece(i, piece) # Block the player's winning move.
+                next_state = self.get_correct_board_representation(piece)
+                if self.turn == 42:
+                    return (next_state * (-1), 0, True) # Draw
+                return (next_state * (-1), 0, False) # The game goes on, a blocking move was made.
+            
+        return None # No blocking move was found.
+
+
 
     def reset(self, agent_moves_first):
         """
@@ -130,12 +201,17 @@ class ConnectFourEnvironment():
         """
         self.turn += 1; piece = -1 if self.turn % 2 == 0 else 1
         state = self.get_correct_board_representation(piece)
-        action = self.opponent_act(state) # Make sure that the opponent sees the board from its own perspective.
-        row = self.drop_piece(action, piece) # Drop the piece in the specified column.
-        next_state = self.get_correct_board_representation(piece) # Clone the board to avoid a problem where the stored board is changed after the board is returned.
         
-        if self.winning_move(piece, action, row): # Opponent won
-            return (next_state * (-1), -1, True) # Rewards are given from the perspective of the agent.
+        win = self.opponent_winning_move(piece)
+        if win is not None: return win # If the opponent has a winning move, it plays it.
+
+        block = self.player_winning_move(piece)
+        if block is not None: return block # If the player has a winning move, the opponent must block it.
+        
+        # If the opponent does not have an immediate winning or blocking move, it plays whatever move it thinks is best.
+        action = self.opponent_act(state) # Make sure that the opponent sees the board from its own perspective.
+        self.drop_piece(action, piece) # Drop the piece in the specified column.
+        next_state = self.get_correct_board_representation(piece) # Clone the board to avoid a problem where the stored board is changed after the board is returned.
         
         if self.turn == 42: # Draw
             return (next_state * (-1), 0, True)
@@ -154,8 +230,9 @@ class ConnectFourEnvironment():
 def print_procedure(env, action):
     print(f'State before action:')
     print_state(env.board.flatten())
+    print(f'Player: {1 if env.turn % 2 == 0 else 2}, Action: {action}')
+    print(f'Turn: {env.turn}')
     state, reward, done = env.step(action)
-    print(f'Player: {2 if env.turn % 2 == 0 else 1}, Action: {action}')
     print(f'Next state:')
     print_state(state)
     print(f'reward: {reward}, done: {done}')
@@ -163,10 +240,18 @@ def print_procedure(env, action):
 if __name__ == '__main__':
     print("You ran the environment file! Good job!")
 
-    # env = ConnectFourEnvironment()
+    Qnetwork = QNetwork(42, 7, 1500)
+    state_dict = torch.load("models/connect4_christian_bigboy.pk1", map_location=torch.device('cpu'))
+    Qnetwork.load_state_dict(state_dict)
+    env = ConnectFourEnvironment(Qnetwork)
 
-    # # Simulating some moves for debugging purposes.
-    # print_procedure(3) # Move 1, Player 1
+    # Simulating some moves for debugging purposes.
+    print_procedure(env, 3) # Move 1, Player 1
+    print_procedure(env, 2) # Move 2, Player 2
+    print_procedure(env, 1) # Move 3, Player 1
+    print_procedure(env, 0) # Move 4, Player 2
+    print_procedure(env, 2) # Move 5, Player 1
+    print_procedure(env, 2) # Move 6, Player 2
     # print_procedure(3) # Move 2, Player 2
     # print_procedure(3) # Move 3, Player 1
     # print_procedure(3) # Move 4, Player 2
